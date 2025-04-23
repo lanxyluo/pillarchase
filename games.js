6e9c1746ef5337b10f8d87eb2f4d67932dee42a7
lanxyluo <kekoukelelang@gmail.com>
2025-04-23 21:01:12 +0800

    Update games.js

--- games.js
+++ games.js
@@ -1,3 +1,11 @@
+// Game data - Pillar Chase
 const games = [
-    // 
+    {
+        id: 1,
+        title: "Space Shooter",
+        thumbnail: "https://via.placeholder.com/300x180/3c5/fff?text=Space+Shooter",
+        category: "action",
+        iframeUrl: "https://html5.gamemonetize.com/mwmz41a58bn4avsyi9t7q6r8va4fu7d4/"
+    },
+    // 复制你原始代码中的所有游戏数据到这里
 ];
