6afd29e4ca38c898a837d5419b1b4bad19215a02
lanxyluo <kekoukelelang@gmail.com>
2025-04-26 18:23:02 +0800

    Update games.js

--- games.js
+++ games.js
@@ -5,42 +5,54 @@ const games = [
         "title": "Castle Craft",
         "thumbnail": "images/Adventure/castle-craft.jpg",
         "category": "adventure",
-        "iframeUrl": "https://www.crazygames.com/embed/castle-craft"
+        "iframeUrl": "https://www.crazygames.com/embed/castle-craft",
+        "description": "Castle Craft is a mystical adventure puzzle game where you merge resources while looking for Fiona's missing husband, Michael, all the while uncovering plenty of mysteries along the way.",
+        "howToPlay": "Step into the wonderous world of Castle Craft, where you play as farmer Fiona, determined on her heartfelt mission to reunite with her husband after a mysterious separation."
     },
     {
         "id": 2,
-        "title": "Firestone Idle RPG",
+        "title": "Firestone Idle Rpg",
         "thumbnail": "images/Adventure/firestone-idle-rpg.jpg",
         "category": "adventure",
-        "iframeUrl": "https://www.crazygames.com/embed/firestone-idle-rpg"
+        "iframeUrl": "https://www.crazygames.com/embed/firestone-idle-rpg",
+        "description": "Firestone Idle RPG is an Idle battle fantasy MMO game set in the world of Alandria. Fight hordes of Undead and Orc enemies in a party of up to five he...",
+        "howToPlay": "As the Hero in this story, your goal is to eliminate the Undead and Orc forces who are wreaking havoc and disorder in Alandria."
     },
     {
         "id": 3,
         "title": "Dead Land Survival",
-        "thumbnail": "images/Puzzles/dead-land-survival.jpg",
+        "thumbnail": "images/Adventure/dead-land-survival.jpg",
         "category": "adventure",
-        "iframeUrl": "https://www.crazygames.com/embed/dead-land-survival"
+        "iframeUrl": "https://www.crazygames.com/embed/dead-land-survival",
+        "description": "Dead Land: Survival is a survival horror game set in a zombie-infested apocalyptic world where you fight for survival with powerful skills you accumulate throughout gameplay.",
+        "howToPlay": "Set out on a treacherous quest through the desolate lands of Dead Land: Survival, where the world has fallen to ruin, and the infected ones lurk around every corner."
     },
-   {
+    {
         "id": 4,
         "title": "Doors Castle",
-        "thumbnail": "images/Puzzles/doors-castle.jpg",
+        "thumbnail": "images/Adventure/doors-castle.jpg",
         "category": "adventure",
-        "iframeUrl": "https://www.crazygames.com/embed/doors-castle"
+        "iframeUrl": "https://www.crazygames.com/embed/doors-castle",
+        "description": "Doors Castle is a thrilling adventure through a haunted fortress filled with secrets, puzzles, and lurking horrors. Every shadow hides danger, every door holds a mystery.",
+        "howToPlay": "Find keys: Unlock doors and dive deeper into the castle’s chilling depths Beware of monsters: Stay hidden and avoid their terrifying gaze Explore the..."
     },
     {
         "id": 5,
         "title": "Stone Grass Mowing Simulator",
         "thumbnail": "images/Casual/stone-grass-mowing-simulator.jpg",
         "category": "casual",
-        "iframeUrl": "https://www.crazygames.com/embed/stone-grass-mowing-simulator"
+        "iframeUrl": "https://www.crazygames.com/embed/stone-grass-mowing-simulator",
+        "description": "Stone Grass: Mowing Simulator is a relaxing yet addictive game where you take control of a powerful mower and transform wild fields into perfectly trimmed landscapes.",
+        "howToPlay": "Upgrade early: Boost your speed and storage first to clear more grass without returning to base as often. Focus on expansion: Unlocking new zones gives you more room and better earnings."
     },
     {
         "id": 6,
         "title": "Supermarket Simulator Cashier Game",
         "thumbnail": "images/Casual/supermarket-simulator-cashier-game.jpg",
         "category": "casual",
-        "iframeUrl": "https://www.crazygames.com/embed/supermarket-simulator-cashier-game"
+        "iframeUrl": "https://www.crazygames.com/embed/supermarket-simulator-cashier-game",
+        "description": "Supermarket Simulator: Store Manager is a dynamic retail experience where you juggle cashier duties and store management.",
+        "howToPlay": "WASD to move Mouse to look around Left shift to Sprint Space to Jump Left click to interact with objects Tab to leave/finish activity E to open/close..."
     },
     {
         "id": 7,
